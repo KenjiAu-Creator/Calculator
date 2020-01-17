bc93a3760a59b94c9cde51e665a7a68fb8887eeb
Kenji <kenji.au9@gmail.com>
2020-01-17 13:17:57 -0700

    Added in Error message for missing arguements

--- code.js
+++ code.js
@@ -163,6 +163,9 @@ DIVIDE.addEventListener('click', () => {
 // Equals
 var EQUAL = document.querySelector('#EQUALS');
 EQUAL.addEventListener('click', () => {
+    if (!xString) {
+        alert("ERROR! Missing arguments for expression")
+    }
     X = xString;
     Y = displayValueArray.join("");
     ans = operate(operator, X, Y);
