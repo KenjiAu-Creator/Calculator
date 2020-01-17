dcaa3cb18ee0a1ff903147c584abbf7c0f5258d0
Kenji <kenji.au9@gmail.com>
2020-01-16 22:52:11 -0700

    Mapping numbers

--- code.js
+++ code.js
@@ -26,3 +26,67 @@ var operate = function(operator, x,  y) {
             break;
     }
 }
+// Mapping number buttons and working display
+var displayValue, displayValueArray, displayValueString;
+displayValueArray = [];
+displayValueString = "";
+var zero = document.querySelector("#zero");
+zero.addEventListener('click', () => {
+    displayValueArray.push(0)
+    displayValueString = displayValueArray.join("");
+    document.getElementById('DISPLAY').innerHTML = displayValueString;
+});
+var one = document.querySelector("#one");
+one.addEventListener('click', () => {
+    displayValueArray.push(1);
+    displayValueString = displayValueArray.join("");
+    document.getElementById('DISPLAY').innerHTML = displayValueString;
+});
+var two = document.querySelector("#two");
+two.addEventListener('click', () => {
+    displayValueArray.push(2);
+    displayValueString = displayValueArray.join("");
+    document.getElementById('DISPLAY').innerHTML = displayValueString;
+});
+var three = document.querySelector("#three");
+three.addEventListener('click', () => {
+    displayValueArray.push(3);
+    displayValueString = displayValueArray.join("");
+    document.getElementById('DISPLAY').innerHTML = displayValueString;
+});
+var four = document.querySelector("#four");
+four.addEventListener('click', () => {
+    displayValueArray.push(4);
+    displayValueString = displayValueArray.join("");
+    document.getElementById('DISPLAY').innerHTML = displayValueString;
+});
+var five = document.querySelector("#five");
+five.addEventListener('click', () => {
+    displayValueArray.push(5);
+    displayValueString = displayValueArray.join("");
+    document.getElementById('DISPLAY').innerHTML = displayValueString;
+});
+var six = document.querySelector("#six");
+six.addEventListener('click', () => {
+    displayValueArray.push(6);
+    displayValueString = displayValueArray.join("");
+    document.getElementById('DISPLAY').innerHTML = displayValueString;
+});
+var seven = document.querySelector("#seven");
+seven.addEventListener('click', () => {
+    displayValueArray.push(7);
+    displayValueString = displayValueArray.join("");
+    document.getElementById('DISPLAY').innerHTML = displayValueString;
+});
+var eight = document.querySelector("#eight");
+eight.addEventListener('click', () => {
+    displayValueArray.push(8);
+    displayValueString = displayValueArray.join("");
+    document.getElementById('DISPLAY').innerHTML = displayValueString;
+});
+var nine = document.querySelector("#nine");
+nine.addEventListener('click', () => {
+    displayValueArray.push(9);
+    displayValueString = displayValueArray.join("");
+    document.getElementById('DISPLAY').innerHTML = displayValueString;
+});
